e71a3952c147c0a9589427b3efc45b3196d65166
EvanMeek <the_lty_mail@foxmail.com>
2022-04-17 00:53:59 +0800

    ui: Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/"></RouterLink>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+</style>
---
+++ src/views/MainSkeleton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="md:h-screen md:w-screen flex flex-col">
+    <div id="nav" class="h-14 w-full bg-zinc-500"></div>
+    <div id="body" class="flex-1 bg-zinc-50 bg-zinc-900 flex box-shadow">
+      <article
+        class="h-1/2 w-5/6 mx-auto my-auto bg-zinc-700 flex box-inner rounded-xl"
+      >
+        <span
+          class="px-8 block mx-auto my-auto text-5xl font-black bg-clip-text text-transparent bg-gradient-to-r from-red-300 via-amber-500 to-green-500"
+          >Drangon Capsule + Veloren + Web3.0 + Ethereum</span
+        >
+      </article>
+    </div>
+    <div id="footer" class="h-1/6 w-full bg-zinc-500"></div>
+  </div>
+</template>
+
+<style scoped></style>
